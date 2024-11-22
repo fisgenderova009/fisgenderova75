5d5b1b49a37c1fb8be34333ade4a5c7703f00605
fisgenderova009 <fisgenderova009@gmail.com>
2024-11-22 15:11:03 +0400

    book sale app created

---
+++ booksale/src/test/java/az/developia/booksale/BooksaleApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.booksale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
