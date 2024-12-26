ea3aa2ea0d27019d008fec0f32eaccdb661c1737
fisgenderova009 <fisgenderova009@gmail.com>
2024-12-26 16:45:36 +0400

    librarian-Fidan-Isgenderova created

---
+++ librarian-Fidan-Isgenderova/src/main/java/az/developia/librarian_Fidan_Isgenderova/LibrarianFidanIsgenderovaApplication.java
@@ -0,0 +1,13 @@
+package az.developia.librarian_Fidan_Isgenderova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarianFidanIsgenderovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarianFidanIsgenderovaApplication.class, args);
+	}
+
+}
